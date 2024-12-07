611c9637220269799245631024a71665d1a4261e
PetersonPHC <petersonhenryque@hotmail.com>
2024-12-07 11:52:53 -0300

    implements curso Forms

--- carometro/src/main/java/br/fateczl/carometro/controller/AlunoHtmlController.java
+++ carometro/src/main/java/br/fateczl/carometro/controller/AlunoHtmlController.java
@@ -1,7 +1,7 @@
 package br.fateczl.carometro.controller;
-
+ 
 import java.util.ArrayList;
-
+ 
 import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,20 +10,27 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
+ 
 import br.fateczl.carometro.model.entities.Aluno;
+import br.fateczl.carometro.model.entities.Curso;
 import br.fateczl.carometro.model.entities.Turma;
 import br.fateczl.carometro.service.implementations.AlunoServiceImp;
+import br.fateczl.carometro.service.implementations.CursoServiceImp;
 import br.fateczl.carometro.service.implementations.TurmaServiceImp;
 import jakarta.validation.Valid;
-
+ 
 @Controller
 public class AlunoHtmlController {
-
+ 
     @Autowired
     private AlunoServiceImp alunoService;
+    
+    @Autowired
     private TurmaServiceImp turmaService;
-
+    
+    @Autowired
+    private CursoServiceImp cursoService;
+ 
     @GetMapping("/alunoGet/{ra}")
     public String alunoGet(@ModelAttribute("ra") String ra, Model model) {
         try {
@@ -42,6 +49,8 @@ public class AlunoHtmlController {
         try {
 			java.util.List<Turma> listTurma = turmaService.buscarTodasAsTurmasExistentes();
 			model.addAttribute("listTurma", listTurma);
+			java.util.List<Curso> listCurso = cursoService.buscarTodos();
+			model.addAttribute("listCurso", listCurso);
 		} catch (ClassNotFoundException e) {
 			System.err.println(e);
 		}
@@ -51,6 +60,7 @@ public class AlunoHtmlController {
     
     @PostMapping("/alunoPost")
     public String alunoPost(@ModelAttribute("aluno") Aluno aluno) {
+    	
     	alunoService.inserir(aluno);
     	return "aluno_inserido";
     }
@@ -64,3 +74,4 @@ public class AlunoHtmlController {
     
     
 }
+ 
