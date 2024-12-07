f1fa2350d5a9fac2447bf3b09ae55a57bf32e181
GeovanniVeloso <geoveloso543@gmail.com>
2024-12-06 21:28:51 -0300

    Primeiras 2 telas feitas

--- carometro/src/main/java/br/fateczl/carometro/controller/AlunoHtmlController.java
+++ carometro/src/main/java/br/fateczl/carometro/controller/AlunoHtmlController.java
@@ -3,10 +3,14 @@ package br.fateczl.carometro.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import br.fateczl.carometro.model.entities.Aluno;
 import br.fateczl.carometro.service.implementations.AlunoServiceImp;
+import jakarta.validation.Valid;
 
 @Controller
 public class AlunoHtmlController {
@@ -27,9 +31,23 @@ public class AlunoHtmlController {
     
     @GetMapping("/alunoPost")
     public String alunoPost(Model model) {
-    	/*Aluno aluno = new Aluno();
-        alunoService.inserir(aluno);*/
-		model.addAttribute("message", "Aluno Cadastrado!");
+    	Aluno aluno = new Aluno();
+        model.addAttribute("aluno", aluno);
         return "alunoPost";
     }
+    
+    @PostMapping("/alunoPost")
+    public String alunoPost(@ModelAttribute("aluno") Aluno aluno) {
+    	alunoService.inserir(aluno);
+    	return "aluno_inserido";
+    }
+    
+    @GetMapping("/home")
+    public String home(Model model) {
+    	model.addAttribute("message", "Isso é um Teste");
+		return "home";
+    	
+    }
+    
+    
 }
---
+++ carometro/src/main/java/br/fateczl/carometro/controller/CursoHTMLController.java
@@ -0,0 +1,9 @@
+package br.fateczl.carometro.controller;
+
+public class CursoHTMLController {
+
+	public CursoHTMLController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
